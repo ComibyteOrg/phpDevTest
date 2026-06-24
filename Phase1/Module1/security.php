c3e5ebc4e398c76abfd60c57e29ed98e4013392d
Oluwadimu Adedeji <comibyteorg@gmail.com>
2026-06-24 11:53:40 +0100

    fix: Add initial security checks

---
+++ Phase1/Module1/security.php
@@ -0,0 +1,3 @@
+<?php 
+    // Security is very important in web development
+    // Security is a process that prevents unauthorized access to resources, such as data or code, from untrusted sources
